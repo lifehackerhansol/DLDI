d4edbb00bf023afcceac6b82a8718d6421aa1867
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2026-05-12 11:41:53 -0700

    do away with EZ5H_CMD_SRAM/SDMC, hardcode directly, hardcode write cmd 0xF0 OR

--- blocksds/source/platform/ez5h/ez5h.h
+++ blocksds/source/platform/ez5h/ez5h.h
@@ -26,13 +26,11 @@
 #define EZ5H_CTRL_READ_512B (EZ5H_CTRL_BASE | MCCNT1_LEN_512)
 
 // EZ5 CARD_COMMANDs
-#define EZ5H_CMD_SRAM (0xB700000000000000ull)
-#define EZ5H_CMD_SDMC (0xB800000000000000ull)
-#define EZ5H_CMD_SDMC_READ_DATA (EZ5H_CMD_SDMC | 0x00F7000000000000ull)
-#define EZ5H_CMD_SDMC_SEND_CRC_STATUS (EZ5H_CMD_SDMC | 0x00F8000000000000ull)
+#define EZ5H_CMD_SDMC_READ_DATA (0xB8F7000000000000ull)
+#define EZ5H_CMD_SDMC_SEND_CRC_STATUS (0xB8F8000000000000ull)
 
 static inline u64 EZ5H_CMD_SDMC_PARAM_CARD(u8 idx, u8 cmd, u32 parameter) {
-    return (EZ5H_CMD_SDMC | 0x00FA000000000000ull | ((u64)idx << 40) | ((u64)cmd << 32) |
+    return (0xB8FA000000000000ull | ((u64)idx << 40) | ((u64)cmd << 32) |
             (u64)parameter);
 }
 
@@ -45,14 +43,18 @@ static inline u64 EZ5H_CMD_SDMC_SEND_CLK(u8 idx) {
     return EZ5H_CMD_SDMC_PARAM_CARD(idx, 0, 0);
 }
 
+// Reads a single u32 from SRAM.
+// B7 XX AA BB CC 00 00 00
+// XX - idx? only 1 is ever used for this value
+// AA BB CC - SRAM address, offset by 0x80000
 static inline u64 EZ5H_CMD_SRAM_READ_DATA(u32 address) {
-    return (EZ5H_CMD_SRAM | (1ull << 48) | ((u64)((address + 0x80000) & 0xFFFFFF) << 24));
+    return (0xB701000000000000ull | ((u64)((address + 0x80000) & 0xFFFFFF) << 24));
 }
 
 static inline u64 EZ5H_CMD_SDMC_WRITE_DATA(const u8* data) {
-    u64 command = (EZ5H_CMD_SDMC | 0x00F6000000000000ull);
-    command |= ((u64)((data[0] >> 4) | 0xF0) << 40) | ((u64)((data[0]) | 0xF0) << 32) |
-               ((u64)((data[1] >> 4) | 0xF0) << 24) | ((u64)((data[1]) | 0xF0) << 16);
+    u64 command = 0xB8F6F0F0F0F00000ull;
+    command |= ((u64)(data[0] >> 4) << 40) | ((u64)(data[0]) << 32) |
+               ((u64)(data[1] >> 4) << 24) | ((u64)(data[1]) << 16);
     return command;
 }
 
